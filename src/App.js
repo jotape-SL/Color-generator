4a8aa5b815e8e76e12ff5bbb09b3897e04f7d4af
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-13 14:08:09 -0300

    clip to the clipboard function

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import SingleColor from './SingleColor';
 import Values from 'values.js';
 
 function App() {
-  const [color, setColor] = useState('#f54260');
+  const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#f54260').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -3,13 +3,24 @@ import React, { useState, useEffect } from 'react';
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexColor);
+      }}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hexColor}</p>
+      {alert && <p className='alert'>copied to clipboard!</p>}
     </article>
   );
 };
